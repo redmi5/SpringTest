abfb9bb8b8a3fb74c3a474a9dfecfa02c5095cef
Roksana <springrainday29@gmail.com>
2018-05-26 16:46:07 +0600

    init

---
+++ src/main/java/kz/almaty/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kz.almaty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/")
+    public String Home(){
+        return "test";
+    }
+
+}
